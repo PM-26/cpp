4a72bd0182a3c599855939dce9747cf1b2185fdb
PM <107318440+PM-26@users.noreply.github.com>
2023-09-14 04:30:49 +0000

    dataTypes

---
+++ dataTypes.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    char a='A';
+    int integer=1;
+    string string="Hey!";
+    bool yes=true;
+
+    cout<<a<<integer<<string<<yes;
+    return 0;
+}
