5b840c9a894760427e5a49dc3a7412532f339d5b
PM <107318440+PM-26@users.noreply.github.com>
2023-09-30 05:36:24 +0000

    Star Plus pattern

---
+++ patterns.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //starPlus
+    
+    // int n;
+
+    // cout << "Enter value of rows:";
+    // cin >> n;
+
+    // int mid = (n / 2) + 1;
+
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= n; j++)
+    //     {
+    //         if (i == mid || j == mid)
+    //         {
+    //             cout << "* ";
+    //         }
+    //         else
+    //         {
+    //             cout << "  ";
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+
+
+
+
+
+
+
+
+    return 0;
+}
