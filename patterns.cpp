e28884a3a0475ad8d62789aeb60a1da02698a9de
PM <107318440+PM-26@users.noreply.github.com>
2023-10-01 16:12:56 +0000

    flipped triangle.

--- patterns.cpp
+++ patterns.cpp
@@ -1,39 +1,50 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main()
-{
-    //starPlus
-    
-    // int n;
+// int main()
+// {
+//     starPlus
 
-    // cout << "Enter value of rows:";
-    // cin >> n;
+//     int n;
 
-    // int mid = (n / 2) + 1;
+//     cout << "Enter value of rows:";
+//     cin >> n;
 
-    // for (int i = 1; i <= n; i++)
-    // {
-    //     for (int j = 1; j <= n; j++)
-    //     {
-    //         if (i == mid || j == mid)
-    //         {
-    //             cout << "* ";
-    //         }
-    //         else
-    //         {
-    //             cout << "  ";
-    //         }
-    //     }
-    //     cout << endl;
-    // }
+//     int mid = (n / 2) + 1;
 
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= n; j++)
+//         {
+//             if (i == mid || j == mid)
+//             {
+//                 cout << "* ";
+//             }
+//             else
+//             {
+//                 cout << "  ";
+//             }
+//         }
+//         cout << endl;
+//     }
 
+//     vertically flipped trianlge
 
+//     cout << "Enter value of n:";
+//     int n;
+//     cin >> n;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= n - i; j++)
+//         {
+//             cout << "  ";
+//         }
+//         for (int k = 1; k <= i; k++)
+//         {
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
 
-
-
-
-
-    return 0;
-}
+//     return 0;
+// }
